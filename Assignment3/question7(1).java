9c5cd3fec78ff6834fbe60da1aa18488c99251f7
Maitry Parikh <maitryparikh23@gmail.com>
2024-03-12 15:05:41 +0530

    Create question7(1).java

---
+++ Assignment3/question7(1).java
@@ -0,0 +1,19 @@
+/*
+Create a package MathPack having class MathDemo with method add() and sub() to
+find addition and subtraction. Create another program and `import package and
+invoke methods.
+*/
+// Name of package to be created
+package MathPack;
+ 
+// Class in which the above created package belong to
+public class MathDemo {
+    public void add(int n1, int n2) {
+      System.out.println(n1+n2);
+    }
+    public void sub(int n1, int n2) {
+      System.out.println(n1-n2);
+    }
+}
+
+
