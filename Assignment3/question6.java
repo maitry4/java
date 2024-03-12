480d89e7fb9513e26b8b39550c668d1083e4fea5
Maitry Parikh <maitryparikh23@gmail.com>
2024-03-12 14:51:55 +0530

    Create question6.java

---
+++ Assignment3/question6.java
@@ -0,0 +1,13 @@
+/*
+Write a program that finds the length of the string “Java Programming”. And also
+display a sub-string formed by the last five characters of the string. ( use String class)
+*/
+class Test {
+  public static void main (String[] args) {
+    String str1 = "Java Programming";
+    System.out.println(str1.length());
+    System.out.println(str1.substring(11));
+  }
+}
+
+
