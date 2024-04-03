914e8e31c421a604a1e7d32c97c796b19773fe45
Maitry Parikh <maitryparikh23@gmail.com>
2024-04-04 05:01:37 +0530

    thread and basic applet

---
+++ Hello.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+import java.applet.*;
+
+/* <applet code="HelloWorld" width=200 height=100>
+
+</applet> */
+
+public class Hello extends Applet
+
+{ public void paint( Graphics g )
+
+{
+
+g.drawString( "Hello World!", 30, 30 );
+
+}
+
+}
